0223f55e808b28cee149e3403e2a42da2d71be64
Omid Tahmasebi <tomid7373@gmail.com>
2023-03-14 02:07:49 +0330

    final commit 1
    final commit 1 for my personal website

--- omidtahmasebi.functions.js
+++ omidtahmasebi.functions.js
@@ -9,6 +9,7 @@ function closenav()
 {
     document.getElementById("mysidenav").style.left="-100%";
     document.getElementById("bar").style.visibility="visible";
+    
 }
 
 
@@ -17,7 +18,11 @@ function openresponsivenav()
     document.getElementById("responsivenav").style.left="0px";
     document.getElementById("responsivebar").style.transform="rotateZ(90deg)";
     document.getElementById("responsivebar").style.transition="0.3s";
+    document.getElementById("responsivenav").style.transition="0.7s";
+    document.getElementById("responsivenav").style.transitionDelay="0.3s";
     document.getElementById("responsivemenuword").style.visibility="hidden";
+    document.getElementById("slides").style.visibility = "hidden";
+    document.getElementById("usefullinks").style.visibility = "hidden";
 
 }
 
@@ -27,4 +32,40 @@ function closeresponsivenav()
     document.getElementById("responsivenav").style.left="-100%";
     document.getElementById("responsivebar").style.transform="rotateZ(0deg)";
     document.getElementById("responsivemenuword").style.visibility="visible";
+    document.getElementById("responsivenav").style.transition="0s";
+    document.getElementById("slides").style.visibility = "visible";
+    document.getElementById("usefullinks").style.visibility = "visible";
+}
+
+var index = 0;
+var fade = setInterval(function(){fading()} , 10)
+setInterval(function(){slider()} , 4000)
+
+
+function slider()
+{
+
+    var slides = document.getElementById("slides").getElementsByTagName("img")
+    ++index;
+
+    if(slides.length == index)
+        index = 0;
+    
+    if(index == 0)
+        slides[slides.length-1].style.display = "none";
+    else
+        slides[index-1].style.display = "none";
+        slides[index].style.display="inline";
+        slides[index].style.opacity = 1;
+        clearInterval(fade)
+        fade = setInterval(function(){fading()} , 10)
+
+}
+function fading()
+{
+    var slides = document.getElementById('slides').getElementsByTagName('img')
+    var opacity = slides[index].style.opacity >=0 ? slides[index].style.opacity : 1;
+    slides[index].style.opacity = opacity-0.0025
+
+
 }
